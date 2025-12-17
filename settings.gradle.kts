b86ae5b8aa5a006386150a0b30e8b8e7c86f5753
이종환 (인턴) <ghksqkdwod0@innogrid.com>
2025-12-17 16:13:06 +0900

    템플릿 모듈 추가

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "miniGames"
+
+include(":modules:KKUTU")
+include(":modules:Oh_Mock")
+include(":modules:catchMind")
+include(":templets")
